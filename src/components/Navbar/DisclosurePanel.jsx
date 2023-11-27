56e96e200e2ef542203cc2707227bb1ddb6d73fe
Lumine <jesterarcas@outlook.com>
2023-11-27 21:39:07 +0800

    minor changes

--- src/components/Navbar/DisclosurePanel.jsx
+++ src/components/Navbar/DisclosurePanel.jsx
@@ -11,34 +11,24 @@ export default function DisclosurePanel() {
   return (
     <Disclosure.Panel className="sm:hidden">
       <div className="space-y-1 px-2 pb-3 pt-2">
-        {navigationCtx.navlist.map((item) => (
-          <Link
-            key={item.name}
-            href={item.href}
-            className={classNames(
-              item.current
-                ? "bg-gray-900 text-white"
-                : "text-gray-300 hover:bg-gray-700 hover:text-white",
-              "block rounded-md px-3 py-2 text-base font-medium"
-            )}
-          >
-            {item.name}
-          </Link>
-          // <Disclosure.Button
-          //   key={item.name}
-          //   as="a"
-          //   href={item.href}
-          //   className={classNames(
-          //     item.current
-          //       ? "bg-gray-900 text-white"
-          //       : "text-gray-300 hover:bg-gray-700 hover:text-white",
-          //     "block rounded-md px-3 py-2 text-base font-medium"
-          //   )}
-          //   aria-current={item.current ? "page" : undefined}
-          // >
-          //   {item.name}
-          // </Disclosure.Button>
-        ))}
+        {navigationCtx.navlist.map((item) => {
+          return (
+            <>
+              <Link
+                key={item.name}
+                href={item.href}
+                className={classNames(
+                  item.current
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "block rounded-md px-3 py-2 text-base font-medium"
+                )}
+              >
+                {item.name}
+              </Link>
+            </>
+          );
+        })}
       </div>
     </Disclosure.Panel>
   );
